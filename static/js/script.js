e22a8bf00313547dabb31cfe72c6e48d19568938
prash9-coder <nimmalaprashanth9@gmail.com>
2024-09-14 13:00:04 +0530

    fixed python requirements

--- static/js/script.js
+++ static/js/script.js
@@ -46,4 +46,13 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log('Product ID:', productId);
     });
 
+    document.addEventListener('DOMContentLoaded', function () {
+      const hamburger = document.getElementById('hamburger');
+      const navMenu = document.getElementById('nav-menu');
+      
+      hamburger.addEventListener('click', function () {
+          navMenu.classList.toggle('active');
+      });
+  });
+  
 
